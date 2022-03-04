3f135c75694257e0ae21d15b2bebb414c51379c5
monirpust <mdmonir1997@gmail.com>
2022-03-04 10:10:51 +0600

    server part added

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket
 import threading
 
 PORT = 5050
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECTED"
 #SERVER = '192.168.0.118' hard coded ip
 SERVER = socket.gethostbyname(socket.gethostname()) #dynamic ip
 ADDR = (SERVER, PORT)
@@ -10,11 +13,28 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 def handle_client(clientsocket, address):
-    pass
+    print(f"[NEW CONNECTION] {address} connected")
+    connected = True
+    while connected:
+        msg_length = clientsocket.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = clientsocket.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{address}] {msg}")
+
+    clientsocket.close()
+
+
 def start():
-    server.lissten()
+    server.listen()
+    print(f"[LISTENING] Server is listening to {SERVER}")
     while True:
         clientsocket, address = server.accept()
+        thread = threading.Thread(target=handle_client, args=(clientsocket,address))
+        thread.start()
+        print(f"[ACTIVE CONNETIONS] {threading.active_count() - 1}")
 
 print("[STARTING] server is starting...")
 start()
